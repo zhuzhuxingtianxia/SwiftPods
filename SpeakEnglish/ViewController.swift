8b7c46fbc9edaabcdb16362bdbb238f2e97e4df9
ZhuzhuXingTianXia <873391579@qq.com>
2017-07-11 11:48:17 +0800

    国际化

--- SpeakEnglish/ViewController.swift
+++ SpeakEnglish/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lebale: UILabel!
+    
+    var lable = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lebale.text = NSLocalizedString("Title", comment: "标题")
+        
+        lable.frame = CGRect.init(x: lebale.frame.minX, y: lebale.frame.maxY+20, width: lebale.frame.width, height: lebale.frame.width)
+        
+        view.addSubview(lable)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        lable.text = NSLocalizedString("commit", comment: "标签")
+        
     }
 
     override func didReceiveMemoryWarning() {
